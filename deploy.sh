9eeee392dc7b1e055815f24dcb3b57208b9f8e27
Voathnak Lim <voathnak.lim@gmail.com>
2020-01-17 16:25:52 +0700

    made it work

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,21 @@
 #!/usr/bin/env bash
 
 
-S3_BUCKET=
+S3_BUCKET=vlim-todo-app-nodejs-bucket
 INPUT_FILE=sam-template.yaml
 OUTPUT_FILE=sam-template-output.yaml
 STAGE_NAME=dev
-STACK_NAME=todo-app-$STAGE_NAME
+STACK_NAME=vlim-todo-app-$STAGE_NAME-second-stack
+PROFILE=voathnakl
+REGION=us-east-1
+
+
+aws s3api --profile $PROFILE create-bucket --bucket $S3_BUCKET --region $REGION
 
 # Copy Swagger file to S3 to be able to transform its content for "deploying" it to API Gateway
-aws s3 cp swagger.yaml s3://$S3_BUCKET/swagger.yaml
+aws s3 	--profile $PROFILE \
+		--region $REGION \
+		cp swagger.yaml s3://$S3_BUCKET/swagger.yaml
 
 cd src
 npm install
@@ -16,9 +23,19 @@ npm run-script lint
 npm prune --production
 cd ..
 
-aws cloudformation package --template-file $INPUT_FILE --output-template-file $OUTPUT_FILE --s3-bucket $S3_BUCKET
-aws cloudformation deploy --template-file $OUTPUT_FILE --stack-name $STACK_NAME --parameter-overrides StageName=$STAGE_NAME S3BucketName=$S3_BUCKET --capabilities CAPABILITY_IAM
+sam package \
+							--profile $PROFILE \
+							--template-file $INPUT_FILE \
+							--output-template-file $OUTPUT_FILE \
+							--s3-bucket $S3_BUCKET
+sam deploy \
+							--profile $PROFILE \
+							--region $REGION \
+							--template-file $OUTPUT_FILE \
+							--stack-name $STACK_NAME \
+							--parameter-overrides StageName=$STAGE_NAME S3BucketName=$S3_BUCKET \
+							--capabilities CAPABILITY_IAM
 
-export API_GATEWAY_URL=$(aws cloudformation describe-stacks --stack-name $STACK_NAME --query 'Stacks[0].Outputs[0].OutputValue' --output text)
+export API_GATEWAY_URL=$(aws cloudformation describe-stacks --profile $PROFILE --region $REGION --stack-name $STACK_NAME --query 'Stacks[0].Outputs[0].OutputValue' --output text)
 
 echo "API Gateway URL: $API_GATEWAY_URL"
